70aaca82348f1bce51321700d0bf40af54503945
Chicle <chicle@protonmail.com>
2022-03-13 19:40:07 +0100

    fix: Faltaba la columna loadout

--- zrp_framework.sql
+++ zrp_framework.sql
@@ -5,6 +5,7 @@ CREATE TABLE `users` (
 	`identifier` VARCHAR(40) NOT NULL,
 	`group` VARCHAR(50) NULL DEFAULT 'user',
 	`inventory` LONGTEXT NULL DEFAULT NULL,
+	`loadout` LONGTEXT NULL DEFAULT NULL,
 	`position` VARCHAR(255) NULL DEFAULT '{"x":-269.4,"y":-955.3,"z":31.2,"heading":205.8}',
 
 	PRIMARY KEY (`identifier`)
