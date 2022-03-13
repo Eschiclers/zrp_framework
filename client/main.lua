3b60a0d5ff032af9bae8e02c561c05e11afe0c3d
Chicle <chicle@protonmail.com>
2022-03-13 19:39:53 +0100

    feat: Llamar al evento onPlayerJoined

---
+++ client/main.lua
@@ -0,0 +1,10 @@
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(0)
+
+		if NetworkIsPlayerActive(PlayerId()) then
+			TriggerServerEvent('zrp_framework:onPlayerJoined')
+			break
+		end
+	end
+end)
